fc4630cfe1f930223330f2242aae9cdb2420e9bc
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-10 00:35:13 +0000

    Jul 10, 2025, 1:35 AM

--- apps/client/src/features/settings/components/SourceCode.tsx
+++ apps/client/src/features/settings/components/SourceCode.tsx
@@ -1,10 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export const SourceCode = () => {
   return (
     <React.Fragment>
-      <Box py={4}></Box>
+      <Box py={4}>
+        <Box px={5}>
+          <Heading letterSpacing={'-3%'}>Source Code</Heading>
+          <Text
+            letterSpacing={'-1%'}
+            color='gray.600'
+          >
+            Manage your source code
+          </Text>
+        </Box>
+      </Box>
     </React.Fragment>
   );
 };
