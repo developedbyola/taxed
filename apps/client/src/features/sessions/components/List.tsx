c01fc39c04038a9fb364c197dc565e9c84366859
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-13 22:29:21 +0000

    Jul 13, 2025, 11:29 PM

--- apps/client/src/features/sessions/components/List.tsx
+++ apps/client/src/features/sessions/components/List.tsx
@@ -32,6 +32,90 @@ const useListSessions = () => {
   return { status: list.status, data: list.data as { sessions: Session[] } };
 };
 
+const Item = ({ session }: { session: Session }) => {
+  const dialog = useDialog();
+  const parser = new UAParser(session.userAgent);
+  const revoke = trpc.sessions.revoke.useMutation({
+    onSuccess: () => {
+      dialog.open({
+        title: 'Session revoked',
+        message: 'Session revoked successfully',
+        actions: [
+          {
+            label: 'Close',
+          },
+        ],
+      });
+    },
+    onError: (error) => {
+      dialog.open({
+        title: 'Unable to revoke session',
+        message: error.message,
+        actions: [
+          {
+            label: 'Close',
+          },
+        ],
+      });
+    },
+  });
+
+  return (
+    <Flex
+      py={3}
+      px={4}
+      gap={1}
+      key={session.id}
+      cursor={'pointer'}
+      flexDirection={'column'}
+      _hover={{ bg: 'gray.200' }}
+      onClick={() =>
+        dialog.open({
+          title: 'Confirm',
+          message: 'Are you sure you want to end this session?',
+          actions: [
+            {
+              label: 'Cancel',
+            },
+            {
+              variant: 'solid',
+              label: 'End session',
+              onClick: () => {
+                revoke.mutate({
+                  sessionId: session.id,
+                });
+              },
+            },
+          ],
+        })
+      }
+      transition={'all 300ms ease-in-out'}
+    >
+      <Box flex={1}>
+        <Heading
+          lineHeight={1}
+          fontSize={14}
+          fontWeight={'medium'}
+        >
+          {`${parser.getOS().name} ${parser.getBrowser().name} - (${
+            session.ipAddress
+          })`}
+        </Heading>
+        <Text
+          mt={2}
+          fontSize={12}
+          lineHeight={1}
+          color='gray.500'
+        >
+          {`Last active - ${dayjs(session.createdAt).format(
+            'DD/MM/YYYY hh:mm A'
+          )}`}
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
+
 export const List = () => {
   const { status, data } = useListSessions();
 
@@ -46,41 +130,11 @@ export const List = () => {
       divideColor='white'
     >
       {data?.sessions.map((session) => {
-        const parser = new UAParser(session.userAgent);
-
         return (
-          <Flex
-            py={3}
-            px={4}
-            gap={1}
+          <Item
+            session={session}
             key={session.id}
-            cursor={'pointer'}
-            flexDirection={'column'}
-            _hover={{ bg: 'gray.200' }}
-            transition={'all 300ms ease-in-out'}
-          >
-            <Box flex={1}>
-              <Heading
-                lineHeight={1}
-                fontSize={14}
-                fontWeight={'medium'}
-              >
-                {`${parser.getOS().name} ${parser.getBrowser().name} ${
-                  parser.getCPU().architecture
-                }`}
-              </Heading>
-              <Text
-                mt={2}
-                fontSize={12}
-                lineHeight={1}
-                color='gray.500'
-              >
-                {`Last active - ${dayjs(session.createdAt).format(
-                  'DD/MM/YYYY hh:mm A'
-                )}`}
-              </Text>
-            </Box>
-          </Flex>
+          />
         );
       })}
     </Box>
