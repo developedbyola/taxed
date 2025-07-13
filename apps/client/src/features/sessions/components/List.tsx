01002ac9727627e4d7fdec344728e19aa5e48ef2
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-13 21:51:41 +0000

    Jul 13, 2025, 10:51 PM

--- apps/client/src/features/sessions/components/List.tsx
+++ apps/client/src/features/sessions/components/List.tsx
@@ -1,3 +1,11 @@
+import { trpc } from '@/libs/trpc';
+
 export const List = () => {
+  const list = trpc.sessions.list.useQuery();
+
+  if (list.status === 'pending') return <div>Loading...</div>;
+
+  if (list.status === 'error') return <div>Error</div>;
+
   return <div>List</div>;
 };
--- apps/server/src/trpc/index.ts
+++ apps/server/src/trpc/index.ts
@@ -3,11 +3,13 @@ import { authRouter } from '@/trpc/routers/auth';
 import { usersRouter } from '@/trpc/routers/users';
 import { systemRouter } from '@/trpc/routers/system';
 import { transactionsRouter } from '@/trpc/routers/transactions';
+import { sessionsRouter } from '@/trpc/routers/sessions';
 
 export const appRouter = router({
   auth: authRouter,
   users: usersRouter,
   system: systemRouter,
+  sessions: sessionsRouter,
   transactions: transactionsRouter,
 });
 
